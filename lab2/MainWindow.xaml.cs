f927f47491cc962c64882785e0c5cc6cdf1d5e5c
Dmitriy <pest2811@gmail.com>
2024-03-06 01:06:26 +0200

    add lab2

---
+++ lab2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, execute_Save, canExecute_Save);
+            //Реєстрація прив'язки
+            CommandBindings.Add(saveCommand);
+
+            CommandBinding openCommandBinding = new CommandBinding(ApplicationCommands.Open, execute_Open, canExecute_Open);
+            CommandBindings.Add(openCommandBinding);
+        }
+
+        void canExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        void execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("d:\\myFile.txt", inputTextBox.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
+   
+        private void canExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        // Execute method for Open command
+        private void execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Open command executed!");
+        }
+
+
+    }
+}
